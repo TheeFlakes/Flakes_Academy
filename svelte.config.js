8e2745e54266e20abdde20ddfb9eb94f90c788b8
Flakes254 <muriuki.denis21@students.dkut.ac.ke>
2024-12-30 11:20:37 +0300

    updates

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
   kit: {
-    // other configurations...
-    vite: {
-      server: {
-        host: '0.0.0.0',
-        port: 3000 // or any other port you want to use
-      }
-    }
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
